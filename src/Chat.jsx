3d7140a5a42d3a08da6044d6e7c3238a703c1020
Caleb Robert Roy <yorbelac@gmail.com>
2024-01-29 19:16:16 -0600

    Some additional styling fixes.

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -3,6 +3,11 @@ import './Chat.css'
 
 function Chat() {
 
+    const sampleMessages = [
+        {role:"user", content:"Something a little more logical and less chaotic here. Assuming a very long message should still be readable. But we do want the CSS styles to be correct."},
+        {role:"assistant", content:"Something a little more logical and less chaotic here. Assuming a very long message should still be readable. But we do want the CSS styles to be correct."},
+    ]
+
     const url = 'https://handy-sculpin-reliably.ngrok-free.app/v1/chat/completions';
     const systemPrompt = { role: 'system', content: 'Answer the prompt briefly, but thoroughly.' }
 
@@ -47,7 +52,7 @@ function Chat() {
         <div className='chat'>
 
             <div className='chat-title'>
-                <h1>WizardLM 1.0</h1>
+                <h1>WizardLM 1.0 via Caleb Roy</h1>
                 <h2>Uncensored Llama2 13B Q5_K_M</h2>
                 <figure class="avatar">
                     <img src="https://tabsapp.s3.amazonaws.com/ai.png" /></figure>
@@ -68,12 +73,6 @@ function Chat() {
             <div class="message-box">
                 <textarea type="text" class="message-input" placeholder="Type message..." value={message} onChange={handleMessageChange}></textarea>
                 <button type='submit' class="submit-button" onClick={handleUserChat}>Send</button>
-            </div>
-            <div className='chat-title'>
-                <h1>Coded and Facilitated by Caleb Roy</h1>
-                <h2>Model trained and tuned by The Bloke (HUGGINGFACE.co)</h2>
-                <figure class="avatar">
-                    <img src="https://tabsapp.s3.amazonaws.com/ai.png" /></figure>
             </div>
         </div>
     );
